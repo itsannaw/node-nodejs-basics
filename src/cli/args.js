3571d1dbe542a8a8b69069b3a2168cb8a681dbba
itsannaw <emikot321@gmail.com>
2024-01-25 21:05:09 +0300

    feat: implement cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const parsedArgs = process.argv.slice(2).reduce((res, arg) => {
+    if (arg.startsWith("--")) {
+      res.push(arg.slice(2));
+    } else {
+      const previousArg = res.pop();
+      res.push(`${previousArg} is ${arg}`);
+    }
+    return res;
+  }, []);
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
