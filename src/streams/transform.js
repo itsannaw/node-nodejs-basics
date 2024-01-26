e27ee6e1039a6a1552c53a3164744031d0211a34
itsannaw <emikot321@gmail.com>
2024-01-26 19:10:56 +0300

    feat: implement streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const reverseStream = new Transform({
+      transform(data, _, callback) {
+        const reversedData = data.toString().split("").reverse().join("");
+        this.push(reversedData);
+        callback();
+      },
+    });
+    console.log("Enter data to be reverses text. Press Ctrl + D to finish");
+    await pipeline(process.stdin, reverseStream, process.stdout);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await transform();
