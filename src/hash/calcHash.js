72a40273e138313888267b43d19d16b22f15bd8b
itsannaw <emikot321@gmail.com>
2024-01-26 19:23:29 +0300

    refactor: change stream API in calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -2,17 +2,16 @@ import fs from "node:fs";
 import crypto from "node:crypto";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+import { pipeline } from "node:stream/promises";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const filePath = resolve(__dirname, "files", "fileToCalculateHashFor.txt");
 
 const calculateHash = async () => {
   try {
-    fs.createReadStream(filePath)
-      .pipe(crypto.createHash("sha256").setEncoding("hex"))
-      .on("finish", function () {
-        console.log(this.read());
-      });
+    const readStream = fs.createReadStream(filePath);
+    const createHash = crypto.createHash("sha256").setEncoding("hex");
+    await pipeline(readStream, createHash, process.stdout);
   } catch (error) {
     console.error(error);
   }
