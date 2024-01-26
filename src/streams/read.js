805024e62abf66f3a062a4cbf9eded321a29c440
itsannaw <emikot321@gmail.com>
2024-01-26 17:12:08 +0300

    feat: implement streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { pipeline } from "node:stream/promises";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const readable = createReadStream(
+      resolve(__dirname, "files", "fileToRead.txt")
+    );
+    await pipeline(readable, process.stdout);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await read();
