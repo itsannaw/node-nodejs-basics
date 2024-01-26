ad69fc3d87b78eb503f47fd622d0660ea5cbf72e
itsannaw <emikot321@gmail.com>
2024-01-26 18:22:40 +0300

    feat: implement streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { pipeline } from "node:stream/promises";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const writable = createWriteStream(
+      resolve(__dirname, "files", "fileToWrite.txt"),
+      { flags: "a" }
+    );
+    process.stdout.write(
+      "Enter data to be written to file. Press Ctrl + D to finish.\n"
+    );
+    await pipeline(process.stdin, writable);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await write();
